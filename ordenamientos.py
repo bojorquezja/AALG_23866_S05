405d8b09502b7f41b1b19a0b8ba33ed4b5506e5e
bojorquezja <bojorquez.ja@gmail.com>
2026-09-14 20:57:35 -0500

    3 metodos de ordenamiento

--- ordenamientos.py
+++ ordenamientos.py
@@ -7,12 +7,32 @@ def ordenSeleccion(lista):
                 ind_min_val = vista
       
         lista[manoIzq], lista[ind_min_val] = lista[ind_min_val], lista[manoIzq]
-      
-        print(f"Paso {manoIzq+1}: {lista}")
-        
     return lista
+
+def ordenBurbuja(lis):
+    for i in range(1, len(lis)):
+        for j in range(len(lis)-1):
+            if lis[j] > lis[j+1]:
+                lis[j], lis[j + 1] = lis[j + 1], lis[j]
+    return lis
+
+def ordenInserccion(lis):
+    for i in range(1, len(lis)):
+        j = i
+        while j > 0 and lis[j - 1] > lis[j]:
+            lis[j], lis[j - 1] = lis[j - 1], lis[j]
+            j -= 1
+    return lis
+
+
 
 numeros = [2, 8, 5, 3, 9, 4, 1]
 print(f"Lista original: {numeros}\n")
 lista_ordenada = ordenSeleccion(numeros)
-print(f"\nLista final ordenada: {lista_ordenada}")
+print(f"\nSeleccion: {lista_ordenada}")
+numeros = [2, 8, 5, 3, 9, 4, 1]
+lista_ordenada = ordenBurbuja(numeros)
+print(f"\nBurbuja: {lista_ordenada}")
+numeros = [2, 8, 5, 3, 9, 4, 1]
+lista_ordenada = ordenInserccion(numeros)
+print(f"\nInserccion: {lista_ordenada}")
